d6d85686cf2bc496982774515fdd27694d9b9a2f
LordSir101 <49959269+LordSir101@users.noreply.github.com>
2020-03-12 16:16:58 -0400

    App setup

---
+++ src/main/java/net/ontariotechu/travelingsalesman/GameActivity.java
@@ -0,0 +1,41 @@
+package net.ontariotechu.travelingsalesman;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //make app fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point); //point contains size of screen
+
+        //initialize view
+        gameView = new GameView(this, point.x, point.y);
+
+        //display view
+        setContentView(gameView);
+    }
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        gameView.resume();
+    }
+}
