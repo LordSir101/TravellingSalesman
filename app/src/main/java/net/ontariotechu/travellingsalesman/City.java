b97fce949b863a5b7904460d96961414e2cce676
LordSir101 <tywunderlich15@gmail.com>
2020-03-17 21:03:24 -0400

    draw cities

---
+++ app/src/main/java/net/ontariotechu/travellingsalesman/City.java
@@ -0,0 +1,15 @@
+package net.ontariotechu.travellingsalesman;
+
+public class City {
+
+    float xPos;
+    float yPos;
+    boolean visited;
+
+    public City (int x, int y) {
+        xPos = x;
+        yPos = y;
+        visited = false;
+    }
+
+}
